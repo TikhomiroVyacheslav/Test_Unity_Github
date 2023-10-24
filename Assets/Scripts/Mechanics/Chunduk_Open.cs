429331c2046231fef31a61176f06fc939cac2174
TikhomiroVyacheslav <v.brudniy@yandex.ru>
2023-10-25 02:03:07 +0300

    Этап 3 по сундукам
    Анимация открытия сундука по кнопке

--- Assets/Scripts/Mechanics/Chunduk_Open.cs
+++ Assets/Scripts/Mechanics/Chunduk_Open.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Chunduk_Open : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] public Animator anim;
+    [SerializeField] public GameObject eForChundick;
+    private bool chundickEntered;
+
+
+
     void Update()
     {
-        
+        if (chundickEntered == true && anim.GetBool("Chest_opened") == false)
+            eForChundick.SetActive(true);
+        else 
+            eForChundick.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.E) && chundickEntered == true)
+        {
+            anim.SetBool("Chest_opened", true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hero"))
+        {
+            chundickEntered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        chundickEntered = false;
     }
 }
