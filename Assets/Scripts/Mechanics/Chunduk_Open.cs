d01d8c6d2f396a22b2933517470fb7f27a730c8e
Zong000 <zongzong108@gmail.com>
2023-11-07 20:55:30 +0300

    Доделал монетки над сундуком, пока не вылетающие

--- Assets/Scripts/Mechanics/Chunduk_Open.cs
+++ Assets/Scripts/Mechanics/Chunduk_Open.cs
@@ -9,6 +9,7 @@ public class Chunduk_Open : MonoBehaviour
     [SerializeField] public GameObject eForChundick;
     [SerializeField] public GameObject Coin20;
     private bool chundickEntered;
+   
 
 
 
@@ -21,7 +22,9 @@ public class Chunduk_Open : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && chundickEntered == true)
         {
             anim.SetBool("Chest_opened", true);
-            GameObject newObject = Instantiate(Coin20, new Vector3(-7.86493778f, -0.429215103f, 0), Quaternion.identity);
+           
+            GameObject newObject = Instantiate(Coin20, transform.localPosition + new Vector3(1.5f, 1.0f, 0.0f), Quaternion.identity);
+          
         }
     }
 
